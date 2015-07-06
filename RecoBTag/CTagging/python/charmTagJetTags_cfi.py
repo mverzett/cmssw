96eaceb0e2288ffa27ec364fbb817c6b6b1d6061
Mauro Verzetti <mverzett@cern.ch>
2015-07-06 12:08:08 -0500

    change names and bugfix

--- RecoBTag/CTagging/python/charmTagJetTags_cfi.py
+++ RecoBTag/CTagging/python/charmTagJetTags_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-charmTagJetTags = cms.EDProducer(
+#pfCombinedSecondaryVertexSoftLeptonCtagLJetTags 
+pfCombinedTMVACtagLJetTags  = cms.EDProducer(
    "JetTagProducer",
    jetTagComputer = cms.string('charmTagsComputer'),
    tagInfos = cms.VInputTag(
