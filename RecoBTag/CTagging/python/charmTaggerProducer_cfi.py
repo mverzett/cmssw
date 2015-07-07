f55ea416b7cca89df8b6a60e87129cabf50f5b19
Mauro Verzetti <mverzett@cern.ch>
2015-07-07 11:01:08 -0500

    added BTD variables

--- RecoBTag/CTagging/python/charmTaggerProducer_cfi.py
+++ RecoBTag/CTagging/python/charmTaggerProducer_cfi.py
@@ -1,10 +1,47 @@
 import FWCore.ParameterSet.Config as cms
+from RecoBTag.CTagging.trainingvars import get_var_pset
 
 charmTagsComputer = cms.ESProducer(
    'CharmTaggerESProducer',
    weightFile = cms.FileInPath('RecoBTag/CTagging/data/c_vs_udsg.weight.xml'),
    variables = cms.VPSet(
-      cms.PSet(name = cms.string('dummy'), default = cms.double(0))
+      #would be cool if we could directly read the TMVA wieight file and get the information from there
+      #the main problem is unpack cms.FileInPath from python, to make it grid-safe
+      get_var_pset("trackSip2dSig_0"),
+      get_var_pset("trackSip2dSig_1"),
+      get_var_pset("trackSip2dSig_2"),
+      get_var_pset("trackSip3dSig_0"),
+      get_var_pset("trackSip3dSig_1"),
+      get_var_pset("trackSip3dSig_2"),
+      get_var_pset("trackSip2dVal_0"),
+      get_var_pset("trackSip2dVal_1"),
+      get_var_pset("trackSip2dVal_2"),
+      get_var_pset("trackSip3dVal_0"),
+      get_var_pset("trackSip3dVal_1"),
+      get_var_pset("trackSip3dVal_2"),
+      get_var_pset("trackJetDist_0"),
+      get_var_pset("trackJetDist_1"),
+      get_var_pset("trackJetDist_2"),
+      get_var_pset("trackDecayLenVal_0"),
+      get_var_pset("trackDecayLenVal_1"),
+      get_var_pset("trackDecayLenVal_2"),
+      get_var_pset("vertexMass_0"),
+      get_var_pset("vertexEnergyRatio_0"),
+      get_var_pset("trackSip2dSigAboveCharm_0"),
+      get_var_pset("trackSip3dSigAboveCharm_0"),
+      get_var_pset("flightDistance2dSig_0"),
+      get_var_pset("flightDistance3dSig_0"),
+      get_var_pset("vertexJetDeltaR_0"),
+      get_var_pset("trackSip2dValAboveCharm_0"),
+      get_var_pset("trackSip3dValAboveCharm_0"),
+      get_var_pset("chargedHadronEnergyFraction"),
+      get_var_pset("massVertexEnergyFraction_0"),
+      get_var_pset("vertexBoostOverSqrtJetPt_0"),
+      get_var_pset("leptonPtRel_0"),
+      get_var_pset("leptonSip3d_0"),
+      get_var_pset("leptonSip3d_1"),
+      get_var_pset("vertexNTracks_0"),
+      get_var_pset("jetNSecondaryVertices")
       ),
    computer = cms.ESInputTag('combinedSecondaryVertexSoftLeptonComputer'),
    tagInfos = cms.VInputTag(
