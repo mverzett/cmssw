dc9a82db4a7785f42474a424b97f7210c3c37217
Robert Bainbridge <bainbrid@users.noreply.github.com>
2018-12-20 21:08:16 +0100

    SuperCluster module

---
+++ RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronSuperClusters_cfi.py
@@ -0,0 +1,10 @@
+
+import FWCore.ParameterSet.Config as cms
+
+lowPtGsfElectronSuperClusters = cms.EDProducer(
+    "LowPtGsfElectronSCProducer",
+    gsfPfRecTracks = cms.InputTag("lowPtGsfElePfGsfTracks"),
+    ecalClusters = cms.InputTag("particleFlowClusterECAL"),
+    hcalClusters = cms.InputTag("particleFlowClusterHCAL"),
+    MaxDeltaR2 = cms.double(1.),
+)
